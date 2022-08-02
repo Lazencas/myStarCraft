e1af386bf1ccf5ac0c5a5cd7e0b085f09c0cbfaa
Guntae Kim <dkfkskgks@naver.com>
2022-08-03 03:21:24 +0900

    2 commit 배경음악구현, 메뉴바 및 종료버튼 구현, 메뉴화면 변경

--- myStarCraft/src/myStarCraft/Main.java
+++ myStarCraft/src/myStarCraft/Main.java
@@ -1,16 +1,18 @@
 package myStarCraft;
 
+import java.io.File;
+
 public class Main {
-    //게임이 실행될 화면의 크기 지정
-	public static final int SCREEN_WIDTH =  1600;
-	public static final int SCREEN_HEIGHT =  800;
-	
-	public static void main(String[] args) {
+	// 게임이 실행될 화면의 크기 지정
+	public static final int SCREEN_WIDTH = 1600;
+	public static final int SCREEN_HEIGHT = 795;
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		//스타크래프트 메인화면에 대한 클래스의 객체 생성
+		// 스타크래프트 메인화면에 대한 클래스의 객체 생성
 		new StarCraft();
-		
+
 	}
 
 }
